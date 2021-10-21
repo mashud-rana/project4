da0f72b187a1e9d7f45646b5147540611c954ee6
mainuddin-sani <main12sani@gmail.com>
2021-10-22 01:22:14 +0600

    done

--- album.php
+++ album.php
@@ -5,43 +5,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Project 4</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div align="center" style="width: 100%;margin: 0;padding: 0;">
-		<div align="center" style="width: 70%;margin: 0;padding: 0;border: 2px solid;">
-			<h2>Project 4</h2>
+	<section class="cc__upload__section--heads">
+	<div class="cc__container">
+		<div class="cc__upload--head">
+			<div class="cc__upload__inner--heads">
+				<h2>Project 4</h2>
 
-			<form style=" width: 100%; " method="POST" action="album.php" enctype="multipart/form-data">
-				<label for="upload_img" style="" for="upload_img">
-					<span style="font-size: 34px;cursor: pointer;background-color: red">Choose Image</span>
-				</label><br><br>
-				<input style="display: none;" id="upload_img" type="file" name="image">
-				<input style="width: 100%; height: 30px;cursor: pointer" type="button" value="Upload" name="upload">
-			</form>
+				<form style="width: 100%;" method="POST" action="album.php" enctype="multipart/form-data">
+					<label for="upload_img" style="">
+						<span class="cc__choose--btn">Choose Image</span>
+					</label>
+					<br>
+					<br>
+					<input style="display: none;" id="upload_img" type="file" name="image">
+					<input class="cc__upload--button" type="button" value="Upload" name="upload">
+				</form>
+			</div>
 		</div>
-	</div>
-	<div>
-		<table width="100%">
-			<?php
-				$img_list = glob("images/*");
-			?>
-			<tr style="padding: 0;margin: 0">
-					<td>
-					<h2>Images List</h2>			
-					<?php
-						foreach ($img_list as $img) {
-							echo "<a href='javascript:void(0)' data_img='".$img."' src='".$img."' onclick=\"display('".$img."')\">".explode("/",$img)[1]."</a><button onclick='download()'>Download</button><br>";
-						}
-					?>
-					</td>
-					<td align="center" rowspan="10">
+		<div class="cc__images__list--heads">
+			<div class="cc__images__inners--left">
+				<?php
+					$img_list = glob("images/*");
+				?>
+				<h2>Image	s List</h2>	
+				<div class="cc__div-auto">
+					<ul>
 						<?php
-							echo "<img id='display_img' src='".$img_list[0]."' style='width:50%'>"
+							foreach ($img_list as $img) {
+								echo "<li><a href='javascript:void(0)' data_img='".$img."' src='".$img."' onclick=\"display('".$img."')\">".explode("/",$img)[1]."</a><button class='cc__dwonload' onclick='download()'>Download</button></li>";
+								
+							}
 						?>
-					</td>
-			</tr>
-		</table>
+					</ul>
+				</div>
+			</div>
+			<div class="cc__images__inners--right">
+				<?php
+					echo "<img id='display_img' src='".$img_list[0]."' style='width:100%'>"
+				?>
+				<div class="cc__image-btn">
+					<button class="cc__dwonload">Download</button>
+				</div>
+			</div>
+			
+		</div>
+		<!-- <div>
+			<table width="100%">
+				
+				<tr style="padding: 0;margin: 0">
+						<td>
+								
+						
+						</td>
+						<td align="center" rowspan="10">
+							
+						</td>
+				</tr>
+			</table>
+		</div> -->
 	</div>
+	</section>
+	
 </body>
 <script type="text/javascript">
 	function display(name)
